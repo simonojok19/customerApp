e6dd81289fd5e24a30839db340d2d8944f9e84ee
simonojok19 <simonojok19@gmail.com>
2020-03-27 08:22:55 +0300

    added customer details component

--- src/app/customers/create-customer/create-customer.component.ts
+++ src/app/customers/create-customer/create-customer.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Customer } from 'src/app/customer';
+import { CustomerService } from '../customer.service';
 
 @Component({
   selector: 'app-create-customer',
@@ -7,9 +9,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateCustomerComponent implements OnInit {
 
-  constructor() { }
+  constructor(private customerService: CustomerService) { }
 
   ngOnInit(): void {
   }
+
 
 }
--- src/app/customers/customer-details/customer-details.component.ts
+++ src/app/customers/customer-details/customer-details.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Customer } from 'src/app/customer';
+import { CustomerService } from '../customer.service';
 
 @Component({
   selector: 'app-customer-details',
@@ -7,9 +9,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CustomerDetailsComponent implements OnInit {
 
-  constructor() { }
+  @Input() customer: Customer;
+
+  constructor(private customerService: CustomerService) { }
 
   ngOnInit(): void {
+  }
+
+  updateActive(isActive: boolean) {
+    this.customerService.updateCustomer(this.customer.key, { active: isActive }).catch(e => console.log(e))
+  }
+
+  deleteCustomer() {
+    this.customerService.deleteCustomer(this.customer.key).catch(e => console.log(e))
   }
 
 }
