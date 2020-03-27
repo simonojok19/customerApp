e64c0a6ae246ab750584ff7e85696121c880beba
simonojok19 <simonojok19@gmail.com>
2020-03-27 08:53:08 +0300

    implemented the customer class

--- src/app/customers/create-customer/create-customer.component.ts
+++ src/app/customers/create-customer/create-customer.component.ts
@@ -8,11 +8,27 @@ import { CustomerService } from '../customer.service';
   styleUrls: ['./create-customer.component.css']
 })
 export class CreateCustomerComponent implements OnInit {
+  customer: Customer = new Customer();
+  submitted = false;
 
   constructor(private customerService: CustomerService) { }
 
   ngOnInit(): void {
   }
 
+  newCustomer(): void {
+    this.submitted = false;
+    this.customer = new Customer();
+  }
+
+  save() {
+    this.customerService.createCustomer(this.customer);
+    this.customer = new Customer();
+  }
+
+  onSubmit() {
+    this.submitted = true;
+    this.save();
+  }
 
 }
