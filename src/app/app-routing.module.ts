c602a488d3e7bdae14e83fb37a96fcb1424b6dd2
simonojok19 <simonojok19@gmail.com>
2020-03-27 09:01:22 +0300

    added the routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CustomersListComponent } from './customers/customers-list/customers-list.component';
+import { CreateCustomerComponent } from './customers/create-customer/create-customer.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'customers', pathMatch: 'full' },
+  { path: 'customers', component: CustomersListComponent },
+  { path: 'add', component: CreateCustomerComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
