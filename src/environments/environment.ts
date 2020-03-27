ef430da1aa14f27ba3fac5ca65ef89c92a74d493
simonojok19 <simonojok19@gmail.com>
2020-03-27 07:30:43 +0300

    added the config

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyBFWROEzI4YZgpEp2XKU-WHioPW75NZopU",
+    authDomain: "customer-app-e3c85.firebaseapp.com",
+    databaseURL: "https://customer-app-e3c85.firebaseio.com",
+    projectId: "customer-app-e3c85",
+    storageBucket: "customer-app-e3c85.appspot.com",
+    messagingSenderId: "192458646791",
+    appId: "1:192458646791:web:84b8bd57183f085d2611a2",
+    measurementId: "G-V9N2S9ZDF6"
+  }
 };
 
 /*
